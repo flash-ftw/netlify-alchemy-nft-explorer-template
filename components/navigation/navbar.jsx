72c591dfec6cb455d4b46df4e9eace706c784f6e
⚡⚡⚡⚡ FLASH ⚡⚡⚡⚡ <44537761+flash-ftw@users.noreply.github.com>
2024-06-01 01:32:34 +0100

    navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -4,10 +4,7 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-				<img
-					className={styles.alchemy_logo}
-					src="/alchemy_logo.svg"
-				></img>
+			
 			</a>
 			<ConnectButton></ConnectButton>
 		</nav>
